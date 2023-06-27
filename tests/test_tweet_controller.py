a1cdaa9c2fe815a6d20e4ed2e8a025b817062ac6
Marcus Vinicius <dasilvactu@yahoo.com.br>
2023-06-27 14:08:40 -0300

    add test if twwet is not success

--- tests/test_tweet_controller.py
+++ tests/test_tweet_controller.py
@@ -10,8 +10,12 @@ class TweetControllerApi((unittest.TestCase)):
     def test_if_post_tweet_success(self):
         client = TweetController()
         try:
-            client.publish_tweet(text='teste 2')
+            client.publish_tweet(text='twwet novo')
         except Exception as e:
             self.fail(f"Error: {str(e)}")
+    def test_if_post_tweet_is_not_success(self):
+        client = TweetController()
+        with self.assertRaisesRegex(Exception, '403 Forbidden'):
+            client.publish_tweet(text='stringddddddd')
 if __name__ == '__main__':
     unittest.main()
